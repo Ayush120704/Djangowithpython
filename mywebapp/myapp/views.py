3655af4190ad7b199bc5af5bcc757835a752bb65
Ayush Mishra <aayumishra2024@gmail.com>
2026-02-16 12:03:42 +0530

    second commit

--- mywebapp/myapp/views.py
+++ mywebapp/myapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("This is the landing page")
+
+def test(request):
+    return HttpResponse("This is self made testing page")
