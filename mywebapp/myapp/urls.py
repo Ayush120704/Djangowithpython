6254671b88bac6a4ca4bc4a64b1c40eceeb5b86c
Ayush Mishra <aayumishra2024@gmail.com>
2026-02-20 19:36:07 +0530

    second commit - created superuser successfully

--- mywebapp/myapp/urls.py
+++ mywebapp/myapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.home, name = 'landing page'),
     path('test/', views.test , name ='test'),
-    # path('show/',views.show , name='show'),
+    path('show/',views.show , name='show'),
 ]
--- mywebapp/myapp/views.py
+++ mywebapp/myapp/views.py
@@ -5,3 +5,6 @@ def home(request):
 
 def test(request):
     return HttpResponse("This is self made testing page")
+
+def show(request):
+    return HttpResponse("This is show page where data of all the other persons would be shown and also with their details")
