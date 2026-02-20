a54b187bdcf22493c598d67709e11901f1f7a7fe
Ayush Mishra <aayumishra2024@gmail.com>
2026-02-20 20:26:01 +0530

    add of amulya

--- mywebapp/myapp/models.py
+++ mywebapp/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class amulya(models.Model):
+    universityrollnumber=models.IntegerField(unique=True)
+    firstname=models.CharField(max_length=255)
+    lastname=models.CharField(max_length=255)
+    email=models.EmailField(max_length=255 , unique=True)
